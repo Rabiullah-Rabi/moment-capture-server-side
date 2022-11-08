e384281d185b3bbfe592203eb85be7fb3163cbe8
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-11-08 13:30:55 +0600

    service service details

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -22,21 +22,26 @@ async function run() {
   try {
     const serviceCollection = client
       .db("moment-capture")
-          .collection("services");
-      
-      //Serve all services
+      .collection("services");
+
+    //Serve all services
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find({});
       const services = await cursor.toArray();
       res.send({ services });
     });
-      //Serve 3 for home services
+    //Serve 3 for home services
     app.get("/servicesHome", async (req, res) => {
       const cursor = serviceCollection.find({});
-        const services = await cursor.limit(3).toArray();
+      const services = await cursor.limit(3).toArray();
       res.send({ services });
     });
-      
+    //Serve service details
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const service = await serviceCollection.findOne({ _id: ObjectId(id) });
+      res.send({ service });
+    });
   } finally {
   }
 }

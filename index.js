a36ab98305cd988a0b2e6b2a491867ee89cabb09
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-11-08 12:29:44 +0600

    connect with mongoDB

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 app.use(express.json());
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('api is running')
-})
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 
-app.listen(port, () => { 
-    console.log('server is running on port',port);
-})
+//mongDB configuration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.o9ekdiy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("moment-capture")
+      .collection("services");
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send({services });
+    });
+  } finally {
+  }
+}
+run();
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});
